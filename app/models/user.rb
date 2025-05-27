d575a7c5458e78a6f8be72ecc74db7aa4329d1f5
Hager Hussein <hussienhagar238@gmail.com>
2025-05-28 01:15:26 +0300

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
-class User < ApplicationRecord
-  has_many :posts
-  has_many :created_posts, class_name: "Post", foreign_key: "creator_id"
-  has_many :post_editors, foreign_key: "editor_id"
-  has_many :edited_posts, through: :post_editors, source: :post
-end
+# class User < ApplicationRecord
+#   has_many :posts
+#   has_many :created_posts, class_name: "Post", foreign_key: "creator_id"
+#   has_many :post_editors, foreign_key: "editor_id"
+#   has_many :edited_posts, through: :post_editors, source: :post
+# end
